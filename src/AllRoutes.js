e73722bc1550f7674a11ae0cfa1b11b87228b4c5
Eduardo <duduprado294@gmail.com>
2022-07-15 19:55:43 -0300

    Faz tudo dos reqs 8 e 9 exceto os testes

--- src/AllRoutes.js
+++ src/AllRoutes.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function AllRoutes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/foods" component={ Recipes } />
-      {/* <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/drinks" component={ Drinks } />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
-      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } /> */}
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
     </Switch>
   );
 }
